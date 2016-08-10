645c493bdff949069ba32ad825116651305f2d98
Timm Heuss <heussd@users.noreply.github.com>
2016-08-10 14:32:42 +0200

    Firefox anti leak settings

---
+++ firefox/Library/Application Support/Firefox/Profiles/default/user.js
@@ -0,0 +1,29 @@
+// Taken from https://github.com/pyllyukko/user.js/blob/master/user.js
+
+// CIS 2.3.4 Block Reported Web Forgeries
+// http://kb.mozillazine.org/Browser.safebrowsing.enabled
+// http://kb.mozillazine.org/Safe_browsing
+// https://support.mozilla.org/en-US/kb/how-does-phishing-and-malware-protection-work
+// http://forums.mozillazine.org/viewtopic.php?f=39&t=2711237&p=12896849#p12896849
+user_pref("browser.safebrowsing.enabled",			true);
+
+// CIS 2.3.5 Block Reported Attack Sites
+// http://kb.mozillazine.org/Browser.safebrowsing.malware.enabled
+user_pref("browser.safebrowsing.malware.enabled",		true);
+
+// Disable safe browsing remote lookups for downloaded files.
+// This leaks information to google.
+// https://www.mozilla.org/en-US/firefox/39.0/releasenotes/
+// https://wiki.mozilla.org/Security/Application_Reputation
+user_pref("browser.safebrowsing.downloads.remote.enabled",	false);
+
+// Disable pocket
+// https://support.mozilla.org/en-US/kb/save-web-pages-later-pocket-firefox
+user_pref("browser.pocket.enabled",				false);
+// https://github.com/pyllyukko/user.js/issues/143
+user_pref("extensions.pocket.enabled", false);
+
+// http://kb.mozillazine.org/Browser.search.suggest.enabled
+user_pref("browser.search.suggest.enabled",			false);
+// Disable "Show search suggestions in location bar results"
+user_pref("browser.urlbar.suggest.searches", false);
