7113f00f33c0695fb13417378353b12c3812e168
Timm <heussd@users.noreply.github.com>
2017-03-26 16:09:31 +0200

    Openbox settings

--- openbox/.config/openbox/autostart.sh
+++ openbox/.config/openbox/autostart.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
-# Load wallpaper
-eval `cat ~/.fehbg` &
+xrandr --output VGA-1 --above LVDS-1
 
-tilda &
-#conky &
-gpg-agent --daemon &
-#psi &
 
 # Load user keycodes
-if [[ -e ~/.Xmodmap.$HOSTNAME ]]; then
-	xmodmap ~/.Xmodmap.$HOSTNAME &
-fi
+#if [[ -e ~/.Xmodmap.$HOSTNAME ]]; then
+#	xmodmap ~/.Xmodmap.$HOSTNAME &
+#fi
 
 #Tune terminal emulation
 #xrdb -load ~/.Xdefaults
 
 #xcompmgr -cC &
-xscreensaver -no-splash &
+#xscreensaver -no-splash &
 
 urxvtd -f -q -o &
-
-#sleep 3
-
-(sleep 2; trayer --edge top --align right --widthtype request --heighttype request --transparent true --alpha 255 --SetPartialStrut false --SetDockType false) &
-
-#(sleep 2; ipager) &
-#(sleep 2; pypanel) &
-tint2 &
+#sleep 2 & trayer --edge top --align right --widthtype request --heighttype request --transparent true --alpha 255 --SetPartialStrut false --SetDockType false &
+glipper &
 
 
-if [ $HOSTNAME == "smithfield" ]; then
-	#thunderbird &
-fi
-
-
-# Is an external screen attached?
-if [[ `xrandr -q | grep 'VGA connected' ` ]]; then
-	enableVGA 1024 768 &	
-	
-	# Reinit wallpaper
-	eval `cat ~/.fehbg` &
-fi
-
-beagled &
+sleep 4 & eval `cat ~/.fehbg`
