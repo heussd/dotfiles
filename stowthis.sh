3e484cbf7f5b214443494355cc9259dae61b87b5
Timm <heussd@users.noreply.github.com>
2017-03-31 17:21:34 +0200

    Introduced OS-specific ignores (trivial implementation)

--- stowthis.sh
+++ stowthis.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-stow -R -v --dir=$(pwd) --target=$HOME --ignore=.DS_Store $*
+STOW_IGNORE="--ignore=.DS_Store"
+
+# Ignore Library-folder for non macOS systems
+uname -s | grep -v Darwin > /dev/null && STOW_IGNORE="$STOW_IGNORE --ignore=Library"
+
+stow --restow --verbose=2 --dir=$(pwd) --target=$HOME $STOW_IGNORE $*
