1e70287d525c2623082d9552a3b5393b619fc608
Timm <heussd@users.noreply.github.com>
2017-03-31 17:51:59 +0200

    Ignored .git

--- stowthis.sh
+++ stowthis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-STOW_IGNORE="--ignore=.DS_Store"
+STOW_IGNORE="--ignore=.DS_Store --ignore=.git"
 
 # Ignore Library-folder for non macOS systems
 uname -s | grep -v Darwin > /dev/null && STOW_IGNORE="$STOW_IGNORE --ignore=Library"
