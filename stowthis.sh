a7e08644fe7041e0de7bc1f62f0d7aca125141c9
Timm <heussd@users.noreply.github.com>
2017-05-13 12:08:04 +0200

    Overwrite everything by default

--- stowthis.sh
+++ stowthis.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 STOW_IGNORE="--ignore=.DS_Store --ignore=.git"
+STOW_OPTS="--restow --override=. --verbose=2"
 
 # Ignore Library-folder for non macOS systems
 uname -s | grep -v Darwin > /dev/null && STOW_IGNORE="$STOW_IGNORE --ignore=Library"
 
-stow --restow --verbose=2 --dir=$(pwd) --target=$HOME $STOW_IGNORE $*
+stow $STOW_OPTS --dir=$(pwd) --target=$HOME $STOW_IGNORE $*
