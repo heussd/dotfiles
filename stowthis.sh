f8beed65ae9087c04c7ccee0879805b0ab51682a
Timm <heussd@users.noreply.github.com>
2017-05-13 13:03:56 +0200

    Support for being called from everywhere, fallback to pwd

--- stowthis.sh
+++ stowthis.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-STOW_IGNORE="--ignore=.DS_Store --ignore=.git"
+STOW_DIR=$(dirname $(readlink -f $0)) || STOW_DIR=$(pwd)
 STOW_OPTS="--restow --override=. --verbose=2"
 
+STOW_IGNORE="--ignore=.DS_Store --ignore=.git"
 # Ignore Library-folder for non macOS systems
 uname -s | grep -v Darwin > /dev/null && STOW_IGNORE="$STOW_IGNORE --ignore=Library"
 
-stow $STOW_OPTS --dir=$(pwd) --target=$HOME $STOW_IGNORE $*
+stow $STOW_OPTS --dir=$STOW_DIR --target=$HOME $STOW_IGNORE $*
