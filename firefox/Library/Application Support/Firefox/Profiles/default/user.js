4b95d8e8b6782ca46b613d7b4985f309bbc806c5
Timm <heussd@users.noreply.github.com>
2017-06-16 17:31:38 +0200

    Alt-Download, Anti-Google measures

--- firefox/Library/Application Support/Firefox/Profiles/default/user.js
+++ firefox/Library/Application Support/Firefox/Profiles/default/user.js
@@ -49,3 +49,28 @@ user_pref("media.block-autoplay-until-in-foreground", true);
 // Enable containers
 //user_pref("privacy.userContext.enabled", true);
 
+user_pref("browser.altClickSave", true);
+
+
+// Taken from https://www.ghacks.net/2015/08/18/a-comprehensive-list-of-firefox-privacy-and-security-settings/
+
+// 0410a: disable "Block dangerous and deceptive content" This setting is under Options>Security
+// in FF47 and under this is was titled "Block reported web forgeries"
+// this covers deceptive sites such as phishing and social engineering
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false); // (FF50+)
+// 0410b: disable "Block dangerous downloads" This setting is under Options>Security
+// in FF47 and under this was titled "Block reported attack sites"
+// this covers malware and PUPs (potentially unwanted programs)
+user_pref("browser.safebrowsing.downloads.enabled", false);
+// disable "Warn me about unwanted and uncommon software" Also under Options>Security (FF48+)
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+// yet more prefs added (FF49+)
+user_pref("browser.safebrowsing.downloads.remote.block_dangerous", false);
+user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", false);
+// 0410c: disable Google safebrowsing downloads, updates
+user_pref("browser.safebrowsing.provider.google.updateURL", ""); // update google lists
+user_pref("browser.safebrowsing.provider.google.gethashURL", ""); // list hash check
+user_pref("browser.safebrowsing.provider.google4.updateURL", ""); // (FF50+)
+user_pref("browser.safebrowsing.provider.google4.gethashURL", ""); // (FF50+)
