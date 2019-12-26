1024adfb48913dc9ea639ca76f201050d6710ddb
Timm Heuss <heussd@users.noreply.github.com>
2019-12-26 11:40:18 +0100

    Cleaned up Firefox configuration

--- Library/Application Support/Firefox/Profiles/default/user.js
+++ Library/Application Support/Firefox/Profiles/default/user.js
@@ -1,35 +1,43 @@
+// ##########################################################
+//   Timm's prefered Firefox Settings
+// Inspired by https://github.com/ghacksuserjs/ghacks-user.js
+// ##########################################################
 
 
-// http://kb.mozillazine.org/Browser.search.suggest.enabled
-user_pref("browser.search.suggest.enabled",			false);
-// Disable "Show search suggestions in location bar results"
-user_pref("browser.urlbar.suggest.searches", false);
 
+// ##########################################################
+//   UI / UX optimisations
+// ##########################################################
+
+// Display more hits in the Awesome Bar
+user_pref("browser.urlbar.maxRichResults", 25);
 
 // Disable mouse wheel zoom
 user_pref("mousewheel.with_control.action", 0);
 user_pref("mousewheel.with_meta.action", 0);
 
-// Disable session restore, legend says that this saves quite some hard disk I/O
-// https://forums.servethehome.com/index.php?threads/firefox-is-chewing-through-your-nand.11346/page-3
-//user_pref("browser.sessionstore.max_tabs_undo", 0);
-//user_pref("browser.sessionstore.max_windows_undo", 0);
-//user_pref("browser.sessionstore.resume_from_crash", false);
-//user_pref("browser.sessionstore.max_resumed_crashes", 0);
-//user_pref("browser.sessionstore.restore_on_demand", false);
-
-
 // Media opened in a new tab is played when the tab is visible
 user_pref("media.block-play-until-visible", true);
 user_pref("media.block-autoplay-until-in-foreground", true);
 
-// Enable containers
-//user_pref("privacy.userContext.enabled", true);
+user_pref("media.videocontrols.picture-in-picture.enabled", "true");
 
 user_pref("browser.altClickSave", true);
 
 
-// Taken from https://www.ghacks.net/2015/08/18/a-comprehensive-list-of-firefox-privacy-and-security-settings/
+// ##########################################################
+//   Privacy Settings
+// ##########################################################
+
+// Enable Containers
+user_pref("privacy.userContext.enabled", true);
+
+user_pref("privacy.donottrackheader.enabled", true);
+
+// http://kb.mozillazine.org/Browser.search.suggest.enabled
+user_pref("browser.search.suggest.enabled", false);
+// Disable "Show search suggestions in location bar results"
+user_pref("browser.urlbar.suggest.searches", false);
 
 // 0410a: disable "Block dangerous and deceptive content" This setting is under Options>Security
 // in FF47 and under this is was titled "Block reported web forgeries"
@@ -51,6 +59,3 @@ user_pref("browser.safebrowsing.provider.google.updateURL", ""); // update googl
 user_pref("browser.safebrowsing.provider.google.gethashURL", ""); // list hash check
 user_pref("browser.safebrowsing.provider.google4.updateURL", ""); // (FF50+)
 user_pref("browser.safebrowsing.provider.google4.gethashURL", ""); // (FF50+)
-
-
-user_pref("media.videocontrols.picture-in-picture.enabled", "true");
