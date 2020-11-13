11727dc6a3854d14f76478a35230441efe9b92fe
Timm Heuss <heussd@users.noreply.github.com>
2020-11-13 03:42:38 +0100

    Add hammerspoon, firefox hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -12,3 +12,18 @@ hs.hotkey.bind({"ctrl", "alt"}, "return", function()
         hs.application.launchOrFocus("kitty")
     end
 end)
+
+hs.hotkey.bind({"ctrl", "alt"}, "f", function()
+    local app = hs.application.get("Firefox")
+    if app then
+        if not app:mainWindow() then
+            app:selectMenuItem({"Firefox", "New Tab"})
+        elseif app:isFrontmost() then
+            app:hide()
+        else
+            app:activate()
+        end
+    else
+        hs.application.launchOrFocus("Firefox")
+    end
+end)
