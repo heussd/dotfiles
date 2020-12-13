6577f34fc5a690049c9741b5adf8a66053b04db2
Timm Heuss <heussd@users.noreply.github.com>
2020-12-14 00:51:50 +0100

    Adjusted hotkeys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,25 @@
+hs.hotkey.bind({"option"}, "space", function()
+    local app = hs.application.get("kitty")
+    if app then
+        if not app:mainWindow() then
+            app:selectMenuItem({"kitty", "New OS window"})
+        elseif app:isFrontmost() then
+            --app:hide()
+        else
+            app:activate()
+        end
+    else
+        hs.application.launchOrFocus("kitty")
+    end
+end)
+
 hs.hotkey.bind({"ctrl", "alt"}, "return", function()
     local app = hs.application.get("kitty")
     if app then
         if not app:mainWindow() then
             app:selectMenuItem({"kitty", "New OS window"})
         elseif app:isFrontmost() then
-            app:hide()
+            --app:hide()
         else
             app:activate()
         end
@@ -19,7 +34,7 @@ hs.hotkey.bind({"command", "shift"}, "f", function()
         if not app:mainWindow() then
             app:selectMenuItem({"Firefox", "New Tab"})
         elseif app:isFrontmost() then
-            app:hide()
+            --app:hide()
         else
             app:activate()
         end
