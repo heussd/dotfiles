48f1f8c9cc696c5439201594242f586c5251a313
Timm Heuss <heussd@users.noreply.github.com>
2020-12-27 13:14:49 +0100

    CAPS -> F20 -> Kitty

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,17 @@
+function say(text) --a helper function to speak something with either VoiceOver or speech synthesis if it's not running
+	if hs.application.get("VoiceOver") ~=nil then
+		
+	hs.osascript.applescript("tell application \"VoiceOver\" to output \"" ..text .."\"")
+	else --VoiceOver is not running, use speech synthesis
+		synth:speak(text)
+		end --VoiceOver running check
+        end --function
+        
+
+hs.hotkey.bind({}, "f20", function()
+    hs.application.launchOrFocus("kitty")
+end)
+
 hs.hotkey.bind({"ctrl", "alt"}, "return", function()
     local app = hs.application.get("kitty")
     if app then
@@ -25,5 +39,21 @@ hs.hotkey.bind({"command", "shift"}, "f", function()
         end
     else
         hs.application.launchOrFocus("Firefox")
+    end
+end)
+
+
+hs.hotkey.bind({"ctrl", "alt"}, "return", function()
+    local app = hs.application.get("kitty")
+    if app then
+        if not app:mainWindow() then
+            app:selectMenuItem({"kitty", "New OS window"})
+        elseif app:isFrontmost() then
+            --app:hide()
+        else
+            app:activate()
+        end
+    else
+        hs.application.launchOrFocus("kitty")
     end
 end)
