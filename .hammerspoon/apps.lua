e760a99db631c4d9e356e3c6b221619ef48f42aa
Timm Heuss <heussd@users.noreply.github.com>
2021-01-28 23:10:00 +0100

    Stickies hotkey

--- .hammerspoon/apps.lua
+++ .hammerspoon/apps.lua
@@ -14,6 +14,14 @@ hyper:bind({}, "m", nil, function()
     hs.application.launchOrFocus("MacVim")
 end)
 
+hyper:bind({}, "a", nil, function()
+    if (hs.application.frontmostApplication():name() == "Stickies") then
+        hs.application.frontmostApplication():hide()
+    else
+        hs.application.launchOrFocus("Stickies")
+    end
+end)
+
 hyper:bind({}, "s", nil, function()
     hs.application.launchOrFocus("Firefox")
     hs.eventtap.keyStroke({"cmd"}, "t")
