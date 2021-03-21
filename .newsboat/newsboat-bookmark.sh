3b5ff02953ece1ec88b12ce2fcbd03ca4ef71493
Timm Heuss <heussd@users.noreply.github.com>
2021-03-21 16:51:20 +0100

    Replace pocket bookmarking with vimwiki

---
+++ .newsboat/newsboat-bookmark.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+url="$1"
+title="$2"
+title=$(echo $title | sed -e 's/ (matches.*$//') 
+tags="$3"
+feed="$4"
+
+file=$(date '+%Y-%m-%d')
+
+cd "$HOME/projects/vimwiki" || exit 1
+
+echo "\n## $title \n
+<$url>" >> ./$file.md
+
+
+git add $file.md
+git commit -m "$file.md (auto commit)" > /dev/null
+
+exit 0
