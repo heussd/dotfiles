76b590678f166c4ef6a056062382539da197b48e
Timm Heuss <heussd@users.noreply.github.com>
2021-03-21 17:02:08 +0100

    Improve browserweiche

--- .newsboat/newsboat-browser.sh
+++ .newsboat/newsboat-browser.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-/usr/bin/firefox "$@" & 2>/dev/null
+if [ -e /Applications/ ]; then
+    open --background "$@" 2>/dev/null &
+    exit 0
+fi
+
+if [ -e /usr/bin/firefox ]; then
+    /usr/bin/firefox "$@" 2>/dev/null &
+    exit 0
+fi
+
+exit 1
