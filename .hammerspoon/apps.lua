bd1eead4b3cd65d368df3eb24018c216f4777216
Timm Heuss <heussd@users.noreply.github.com>
2021-04-29 18:18:01 +0200

    Add Signal

--- .hammerspoon/apps.lua
+++ .hammerspoon/apps.lua
@@ -6,20 +6,17 @@ hyper:bind({}, "e", nil, function()
     hs.application.launchOrFocus("Finder")
 end)
 
-hyper:bind({}, "f", nil, function()
-    hs.application.launchOrFocus("Firefox")
+hyper:bind({}, "i", nil, function()
+    hs.application.launchOrFocus("Signal")
 end)
 
 hyper:bind({}, "m", nil, function()
     hs.application.launchOrFocus("MacVim")
 end)
 
-hyper:bind({}, "a", nil, function()
-    if (hs.application.frontmostApplication():name() == "Stickies") then
-        hs.application.frontmostApplication():hide()
-    else
-        hs.application.launchOrFocus("Stickies")
-    end
+
+hyper:bind({}, "f", nil, function()
+    hs.application.launchOrFocus("Firefox")
 end)
 
 hyper:bind({}, "s", nil, function()
@@ -27,6 +24,7 @@ hyper:bind({}, "s", nil, function()
     hs.eventtap.keyStroke({"cmd"}, "t")
     hs.eventtap.keyStroke({"cmd"}, "v")
 end)
+
 
 hs.hotkey.bind({"alt"}, "space", function()
     hs.execute("~/.scripts/snippets-choose", false)
