c89964f7001c036e94497a0f57ec3c8a5bbb95d1
Timm Heuss <heussd@users.noreply.github.com>
2021-06-18 19:57:02 +0200

    Add Lock Hook

--- .hammerspoon/sleepwatcher.lua
+++ .hammerspoon/sleepwatcher.lua
@@ -6,6 +6,13 @@ local function pull()
 end
 
 function sleepWatch(eventType)
+    if (eventType == hs.caffeinate.watcher.screensDidLock) then
+        print("Screen lock detected")
+		hs.execute("killall newsboat", true)
+        hs.execute("make -f $HOME/Makefile push", true)
+        hlspeak('doop')
+    end
+
     if (eventType == hs.caffeinate.watcher.screensDidUnlock) then
         print("Wake-up detected")
 		hlspeak('bell')
