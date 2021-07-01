594a71b18d9d3ce2dbb77aa0f7bf36526b9603da
Timm Heuss <heussd@users.noreply.github.com>
2021-07-01 20:13:11 +0200

    Disable https rewrites

--- .finicky.js
+++ .finicky.js
@@ -3,13 +3,6 @@ options: {
     hideIcon: true,
 },
 defaultBrowser: "Firefox",
-rewrite: [
-    {
-      // Redirect all urls to use https
-      match: ({ url }) => url.protocol === "http",
-      url: { protocol: "https" }
-    }
-  ],
 handlers: [
     {
         match: finicky.matchHostnames(["pwc.com", "accounts.google.com", "www.google.com", "stream.meet.google.com", "mail.google.com", "drive.google.com", "calendar.google.com", "chat.google.com", "docs.google.com"]),
