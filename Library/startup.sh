fc86b6e9042a33d5f2ea110bdd95592b76cb1ce9
Timm Heuss <heussd@users.noreply.github.com>
2021-08-09 23:23:56 +0200

    Add startup script

---
+++ Library/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+open -a "Dozer.app"
+open -a "Tiles.app"
+open -a "Yippy.app"
+open -a "Hammerspoon.app"
+open -a "Easy Move+Resize.app"
+open -a "Docker.app"
+open -a "Next Meeting.app"
