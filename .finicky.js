f85c15146861275544c3401717603f539e7db46e
Timm Heuss <heussd@users.noreply.github.com>
2021-11-04 19:54:14 +0100

    Support MS Teams

--- .finicky.js
+++ .finicky.js
@@ -1,24 +1,28 @@
 module.exports = {
-options: {
-    hideIcon: true,
-},
-defaultBrowser: "Firefox",
-handlers: [
-    {
-        match: finicky.matchHostnames(["pwc.com", "accounts.google.com", "www.google.com", "stream.meet.google.com", "mail.google.com", "drive.google.com", "calendar.google.com", "chat.google.com", "docs.google.com"]),
-        browser: "Google Chrome"
+    options: {
+        hideIcon: true,
     },
-    {
-        // Open Google Meet in app mode
-        match: finicky.matchHostnames(["meet.google.com"]),
-        browser: (options) => ({
-            name: "Google Chrome",
-            args: [ "--app=" + options.urlString ]
-        })
-    },
-    {
-        match: finicky.matchHostnames(["teams.microsoft.com"]),
-        browser: "Google Chrome"
-    },
-]
+    defaultBrowser: "Firefox",
+    handlers: [
+        {
+            match: ["*.pwc.com", "*.pwc.de"],
+            browser: "Google Chrome"
+        },
+        {
+            match: finicky.matchHostnames(["accounts.google.com", "www.google.com", "stream.meet.google.com", "mail.google.com", "drive.google.com", "calendar.google.com", "chat.google.com", "docs.google.com"]),
+            browser: "Google Chrome"
+        },
+        {
+            // Open Google Meet in app mode
+            match: finicky.matchHostnames(["meet.google.com"]),
+            browser: (options) => ({
+                name: "Google Chrome",
+                args: [ "--app=" + options.urlString ]
+            })
+        },
+        {
+            match: finicky.matchHostnames(["teams.microsoft.com"]),
+            browser: "Microsoft Teams"
+        },
+    ]
 };
