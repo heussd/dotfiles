757c52db85edd56ba935d535827da9c2e6409968
Timm Heuss <heussd@users.noreply.github.com>
2022-10-23 15:25:24 +0200

    Add OS_ARCH check, folder checks

--- .onboard-dotfiles.sh
+++ .onboard-dotfiles.sh
@@ -5,6 +5,8 @@ set -o pipefail
 set -o nounset
 
 OS_NAME=$( uname -s | tr '[:upper:]' '[:lower:]')
+OS_ARCH=$( uname -m )
+
 
 function installHomebrew {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
@@ -14,11 +16,6 @@ function coGitBare {
 	DOTFILES_REPO="$1"
 	DOTFILES_WORKDIR="$2"
 	DOTFILES_BARE="$2/$3"
-
-	echo "$DOTFILES_REPO"
-	echo "$DOTFILES_WORKDIR"
-	echo "$DOTFILES_BARE"
-	echo "$OS_NAME"
 
 	git clone --bare "$DOTFILES_REPO" "$DOTFILES_BARE/"
 	git --git-dir="$DOTFILES_BARE" config --local status.showUntrackedFiles no
@@ -46,22 +43,52 @@ function coGitBare {
 	touch "$DOTFILES_BARE/FETCH_HEAD"
 }
 
-case "$OS_NAME" in
-  darwin*)
-	xcode-select --install || true
-	;; 
-  linux*)
-  	sudo apt install -y curl make git
-  	;;
-  *)
-	echo "Unsupported OS: $(uname)"
-	exit 1
-	;;
-esac
 
+if ! command -v "git" &> /dev/null; then
+	case "$OS_NAME" in
+	darwin*)
+		xcode-select --install || true
+		;; 
+	linux*)
+		sudo apt install -y curl make git
+		;;
+	*)
+		echo "Unsupported OS: $(uname)"
+		exit 1
+		;;
+	esac
+else
+	echo "Skipping basics installation as they are already installed."
+fi
 
 cd "$HOME" || exit 1
 
-installHomebrew
-coGitBare "https://github.com/heussd/dotfiles.git" "$HOME/" ".dotfiles-bare-repo/"
-coGitBare "https://github.com/heussd/dotfiles-private" "$HOME/" ".dotfiles-private-bare-repo/"
+
+if ! command -v "brew" &> /dev/null; then
+	case "$OS_ARCH" in
+		aarch64)
+			echo "Skipping homebrew installation as it is unsupported on $OS_ARCH"
+			echo "https://docs.brew.sh/Homebrew-on-Linux#arm-unsupported"
+			;;
+		*)
+			installHomebrew
+			;;
+	esac
+else
+	echo "Skipping homebrew installation as it is already installed."
+fi
+
+
+if [ ! -d ".dotfiles-bare-repo/" ]; then
+	coGitBare "https://github.com/heussd/dotfiles.git" "$HOME/" ".dotfiles-bare-repo/"
+	echo "Onboarding complete. Re-execute for private dotfiles."
+else
+	echo "Skipping dotfiles onboarding as it is already onboarded"
+fi
+
+
+if [ ! -d ".dotfiles-private-bare-repo/" ]; then
+	coGitBare "https://github.com/heussd/dotfiles-private" "$HOME/" ".dotfiles-private-bare-repo/"
+else
+	echo "Skipping private dotfiles onboarding as it is already onboarded"
+fi
