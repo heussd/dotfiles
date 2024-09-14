de8e3c24dbd96cb05755f9b1e578cab84670438a
Timm Heuss <heussd@users.noreply.github.com>
2024-09-14 14:04:51 +0200

    Highlight currently focussed window

---
+++ .hammerspoon/focussed-border.lua
@@ -0,0 +1,24 @@
+function redrawBorder()
+    win = hs.window.focusedWindow()
+    if win ~= nil then
+        top_left = win:topLeft()
+        size = win:size()
+        if global_border ~= nil then
+            global_border:delete()
+        end
+        global_border = hs.drawing.rectangle(hs.geometry.rect(top_left['x'], top_left['y'], size['w'], size['h']))
+        global_border:setStrokeColor({["red"]=1,["blue"]=1,["green"]=0,["alpha"]=0.8})
+        global_border:setFill(false)
+        global_border:setStrokeWidth(20)
+        global_border:show()
+        global_border:hide(1)
+    end
+end
+
+redrawBorder()
+
+allwindows = hs.window.filter.new(nil)
+allwindows:subscribe(hs.window.filter.windowCreated, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowFocused, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowMoved, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowUnfocused, function () redrawBorder() end)
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -24,27 +24,9 @@ hs.menuIcon(false)
 hs.consoleOnTop(false)
 
 
--- Highlight current window
--- Taken from https://github.com/shuxiao9058/dothammerspoon/blob/52e53d665c577f33235d92cf17ee8c74c924d5a2/windowHighlight.lua
-
-hs.window.highlight.ui.overlay=true
-hs.window.highlight.ui.overlayColor = {0, 0, 0, 0.3}
-hs.window.highlight.ui.isolateColor = {0, 0, 0, 0.9}
-hs.window.highlight.ui.isolateColorInverted = {1,1,1,0.95}
-
-hs.window.highlight.ui.frameWidth = 10
-hs.window.highlight.ui.frameColor = {0,0,1,0.2}
-hs.window.highlight.ui.frameColorInvert = {1,0.4,0,0.5}
-
-hs.window.highlight.ui.flashDuration = 2
-hs.window.highlight.ui.windowShownFlashColor = {0,1,0,0.8}
-hs.window.highlight.ui.windowHiddenFlashColor = {1,0,0,0.8}
-hs.window.highlight.ui.windowShownFlashColorInvert = {1,0,1,0.8}
-hs.window.highlight.ui.windowHiddenFlashColorInvert = {0,1,1,0.8}
-
-
-
 require('utils')
+
+require('focussed-border')
 
 require('apps-hotkeys')
 require('vim-cursor')
