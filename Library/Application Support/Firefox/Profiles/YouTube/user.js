f2b313c72c627576d050293f0059601a821c1d6b
Timm Heuss <heussd@users.noreply.github.com>
2025-01-29 23:54:50 +1100

    Introduce profile split

---
+++ Library/Application Support/Firefox/Profiles/YouTube/user.js
@@ -0,0 +1,15 @@
+
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.uidensity", 1);
+user_pref("browser.fullscreen.autohide", true);
+
+
+user_pref("browser.startup.page", 3);
+user_pref("extensions.pocket.enabled", false);
+
+
+user_pref("browser.bookmarks.addedImportButton", true);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+user_pref("browser.warnOnQuit", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
